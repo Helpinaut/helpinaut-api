4484ccab2ee4aff0d21ac7410d60ccb09855b914
Miguel Fernandez <miguelferlez@gmail.com>
2025-10-26 16:09:10 +0100

    chore: change main config

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+
+  app.setGlobalPrefix('api');
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Helpinaut API')
     .setDescription(
-      'REST API Backend for Helpinaut. Built with NestJS, Prisma and PostgreSQL.',
+      'REST API Documentation for Helpinaut. Built with NestJS, Prisma and PostgreSQL.',
     )
     .setVersion('0.1')
     .build();
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
 
-  SwaggerModule.setup('api', app, swaggerDocument);
+  SwaggerModule.setup('swagger', app, swaggerDocument, {
+    customSiteTitle: 'Helpinaut API | Swagger',
+  });
 
   app.enableCors();
   app.useGlobalPipes(
@@ -28,6 +33,7 @@ async function bootstrap() {
 
   await app.listen(port, () => {
     console.log(`Helpinaut API running in port ${port}`);
+    console.log(`Try me in http://localhost:${port}/swagger`);
   });
 }
 
