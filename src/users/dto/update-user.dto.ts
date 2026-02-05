ccf899adc9bd6da0095d094c9d625d2a387191ac
Miguel Fernandez <miguelferlez@gmail.com>
2026-02-05 13:29:37 +0100

    style: add custom error messages in update user dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -14,18 +14,18 @@ import {
 export class UpdateUserDto {
   @ApiPropertyOptional()
   @IsOptional()
-  @IsEmail()
+  @IsEmail(undefined, { message: 'Invalid email format' })
   email?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
-  @MaxLength(16)
+  @MaxLength(16, { message: 'Username can not be longer than 16 characters' })
   username?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
-  @MinLength(8)
+  @MinLength(8, { message: 'Password must be longer than 8 characters' })
   password?: string;
 }
