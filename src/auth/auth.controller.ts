87d8462a99a478b91cf1dacb8faea7ec99548b17
Miguel Fernandez <miguelferlez@gmail.com>
2026-03-04 13:34:41 +0100

    fix: expected login successful response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
@@ -11,6 +11,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(200)
   @ApiOkResponse({ description: 'JWT access token successfully generated' })
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
